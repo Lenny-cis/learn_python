f53e4f3f50d3f59990f8bf084a71c7eae6f8151a
Lenny-cis <ljn9008@163.com>
2017-12-27 23:32:45 +0800

    c9 - 171227

--- C9.py
+++ C9.py
@@ -18,4 +18,45 @@ for r in range(sheet.nrows):
 title_rows=zip(sheet.row_values(4),sheet.row_values(5))
 titles=[t[0]+' '+t[1] for t in title_rows]
 titles=[t.strip() for t in titles]
-print(titles)
+titles
+country_rows=[sheet.row_values(r) for r in range(6,114)]
+
+from xlrd.sheet import ctype_text
+text_type=agate.Text()
+number_type=agate.Number()
+boolean_type=agate.Boolean()
+date_type=agate.Date()
+example_row=sheet.row(6)
+print(example_row)
+print(example_row[0].ctype)
+print(example_row[0].value)
+print(ctype_text)
+types=[]
+for v in example_row:
+    value_type=ctype_text[v.ctype]
+    if value_type=='text':
+        types.append(text_type)
+    elif value_type=='number':
+        types.append(text_type)
+    elif value_type=='xldate':
+        types.append(text_type)
+    else:
+        types.append(text_type)
+table=agate.Table(country_rows,titles,types)
+table.print_table(max_columns=7)
+def remove_bad_chars(val):
+    if val=='-':
+        return None
+    return val
+cleaned_rows=[]
+for row in country_rows:
+    cleaned_row=[remove_bad_chars(rv) for rv in row]
+    cleaned_rows.append(cleaned_row)
+    
+def get_new_array(old_array,function_to_clean):
+    new_arr=[]
+    for row in old_array:
+        cleaned_row=[function_to_clean(rv) for rv in row]
+        new_arr.append(cleaned_row)
+    return new_arr
+cleaned_rows=get_new_array(country_rows,remove_bad_chars)
